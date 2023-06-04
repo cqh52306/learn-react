2dc3475edd980537d0d83febd9acd02290f3916a
qinghua.cao <17959400@qq.com>
2023-06-04 15:40:42 +0800

    新旧jsx的区别以及vite初始化

---
+++ doc/1.oldjsx.js
@@ -0,0 +1,16 @@
+const babel = require("@babel/core");
+const sourceCode = `
+<h1>
+    hello<span style={{ color: "red" }}>world</span>
+</h1>
+`;
+const result = babel.transform(sourceCode, {
+  plugins: [["@babel/plugin-transform-react-jsx", { runtime: "classic" }]],
+});
+console.log(result.code);
+
+// React.createElement("h1", null, "hello", React.createElement("span", {
+//   style: {
+//     color: "red"
+//   }
+// }, "world"));
