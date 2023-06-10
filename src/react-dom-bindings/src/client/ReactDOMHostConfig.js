4fd3f53d1d8ba0fedb1eab65aad5888acc88a327
qinghua.cao <17959400@qq.com>
2023-06-10 19:08:32 +0800

    feat: commit-root

--- src/react-dom-bindings/src/client/ReactDOMHostConfig.js
+++ src/react-dom-bindings/src/client/ReactDOMHostConfig.js
@@ -17,3 +17,10 @@ export const createTextInstance = (content) => {
 export function finalizeInitialChildren(domElement, type, props) {
   setInitialProperties(domElement, type, props);
 }
+
+export function appendChild(parentInstance, child) {
+  parentInstance.appendChild(child);
+}
+export function insertBefore(parentInstance, child, beforeChild) {
+  parentInstance.insertBefore(child, beforeChild);
+}
