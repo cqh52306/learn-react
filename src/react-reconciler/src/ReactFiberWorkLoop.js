d1b86729ff19aeb1c0412e83f91de6a2e07b1f92
qinghua.cao <17959400@qq.com>
2023-06-10 15:18:32 +0800

    feat: commitRoot

--- src/react-reconciler/src/ReactFiberWorkLoop.js
+++ src/react-reconciler/src/ReactFiberWorkLoop.js
@@ -2,6 +2,8 @@ import { scheduleCallback } from "scheduler";
 import { createWorkInProgress } from "./ReactFiber";
 import { beginWork } from "./ReactFiberBeginWork";
 import { completeWork } from "./ReactFiberCompleteWork";
+import { MutationMask, NoFlags } from "./ReactFiberFlags";
+import { commitMutationEffectsOnFiber } from "./ReactFiberCommitWork";
 let workInProgress = null;
 
 /**
@@ -29,6 +31,93 @@ function performConcurrentWorkOnRoot(root) {
   // 第一次渲染以同步的方式渲染根节点，初次渲染的时候，都是同步的
   renderRootSync(root);
   console.log("root", root);
+  // 开始进入提交阶段，就是执行副作用，修改真实DOM
+  const finishedWork = root.current.alternate;
+  printFiber(finishedWork);
+  console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+  root.finishedWork = finishedWork;
+  commitRoot(root);
+}
+
+function commitRoot(root) {
+  const { finishedWork } = root;
+  // 判断子树有没有副作用
+  const subtreeHasEffects =
+    (finishedWork.subtreeFlags & MutationMask) !== NoFlags;
+  const rootHasEffect = (finishedWork.flags & MutationMask) !== NoFlags;
+  // 如果自己的副作用或者子节点有副作用就进行提交DOM操作
+  if (subtreeHasEffects || rootHasEffect) {
+    console.log("commitRoot", finishedWork.child);
+    commitMutationEffectsOnFiber(finishedWork, root);
+  }
+  // 等DOM变更后，就可以让root的current指向新的fiber树
+  root.current = finishedWork;
+}
+function printFiber(fiber) {
+  /*
+    fiber.flags &= ~Forked;
+    fiber.flags &= ~PlacementDEV;
+    fiber.flags &= ~Snapshot;
+    fiber.flags &= ~PerformedWork;
+    */
+  if (fiber.flags !== 0) {
+    // console.log(
+    //   getFlags(fiber.flags),
+    //   getTag(fiber.tag),
+    //   typeof fiber.type === "function" ? fiber.type.name : fiber.type,
+    //   fiber.memoizedProps
+    // );
+    if (fiber.deletions) {
+      for (let i = 0; i < fiber.deletions.length; i) {
+        const childToDelete = fiber.deletions[i];
+        console.log(
+          getTag(childToDelete.tag),
+          childToDelete.type,
+          childToDelete.memoizedProps
+        );
+      }
+    }
+  }
+  let child = fiber.child;
+  while (child) {
+    printFiber(child);
+    child = child.sibling;
+  }
+}
+function getTag(tag) {
+  switch (tag) {
+    case FunctionComponent:
+      return `FunctionComponent`;
+    case HostRoot:
+      return `HostRoot`;
+    case HostComponent:
+      return `HostComponent`;
+    case HostText:
+      return HostText;
+    default:
+      return tag;
+  }
+}
+function getFlags(flags) {
+  if (flags === (Update | Placement | ChildDeletion)) {
+    return `自己移动和子元素有删除`;
+  }
+  if (flags === (ChildDeletion | Update)) {
+    return `自己有更新和子元素有删除`;
+  }
+  if (flags === ChildDeletion) {
+    return `子元素有删除`;
+  }
+  if (flags === (Placement | Update)) {
+    return `移动并更新`;
+  }
+  if (flags === Placement) {
+    return `插入`;
+  }
+  if (flags === Update) {
+    return `更新`;
+  }
+  return flags;
 }
 
 function prepareFreshStack(root) {
